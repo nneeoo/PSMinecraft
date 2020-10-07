dc74c0f608c14eac15a042dd88f2444a7fdef94a
Ilya Machekhin <nneeoo@live.ru>
2020-10-08 01:48:36 +0300

    even better

--- Approve-MinecraftEULA/Approve-MinecraftEULA.psm1
+++ Approve-MinecraftEULA/Approve-MinecraftEULA.psm1
@@ -6,16 +6,13 @@ function Approve-MinecraftEULA {
     )
     $EULAfile = (Join-Path $MinecraftPath eula.txt)
     $Verb = (Get-Content $EULAfile -ErrorAction Stop) -split "\n"
-
-    if(Test-Path $EULAfile)
-    {
-        Write-Error -Category ObjectNotFound -TargetObject  $EULAfile -Message "File not found"
-       
-    }
     $Verb = $Verb[0].TrimStart('#')
+    
     if ($PSCmdlet.ShouldProcess($EULAfile, $Verb, 'Approve-MinecraftEULA')) {
 
         (Get-Content $EULAfile) -replace "false", "true" | Out-File $EULAfile
         Write-Output ("Approved agreement on minecraft server EULA in ( " + $MinecraftPath + " ) folder")
     }
 }
+
+Approve-MinecraftEULA
